cb15186a07a7f6188b25fb9b598dacea9cc3f8a0
tatijan <34741441+tatijan@users.noreply.github.com>
2018-02-16 20:01:31 +0200

    Delete del.php

--- del.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../src/functions.php';
-if (!isset($_SESSION['admin'])) {
-    header("Refresh: 2; url='index.php'");
-    echo 'нет доступа';
-    exit;
-}
-array_map('unlink', glob("../Tests/*"));
-redirect('list');
